3c7208b6e04b14f39ad7a0fade90dc6e1965cade
Alessandrr <107347838+Alessandrr@users.noreply.github.com>
2023-05-01 17:09:34 +0400

    Working red circle

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    private func setupLight(for view: UIView) {
+        print(view.bounds.width)
+        view.layer.cornerRadius = view.bounds.width / 2
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // Set corner radius of view to create circle
+        redLightView.layer.cornerRadius = min(redLightView.frame.width, redLightView.frame.height) / 2
+    }
 }
 
